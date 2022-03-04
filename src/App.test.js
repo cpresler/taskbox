d7ab9042ada6f431063f58620656d1fcf0d085c8
Christy Presler <cpresler@users.noreply.github.com>
2022-03-04 11:16:16 -0600

    add inbox screen mock service worker, and component interactions

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders inbox screen', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const InboxScreen = screen.getAllByTestId('inbox')[0];
+  expect(InboxScreen).toBeInTheDocument();
 });
